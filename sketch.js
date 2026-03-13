b339ac75e673f1d942c5bd33f62f72e76f18711b
Angela B <jb8161@nyu.edu>
2026-03-13 15:05:59 -0400

    changed noodle and fixed some ratio

--- sketch.js
+++ sketch.js
@@ -39,24 +39,24 @@ function draw() {
         x:mouseX,
         y:mouseY,
         alpha:255,
-        size: random(8,15)
+        size: random(2,15)
     };
     trail.push(newStar);
     starCount++;
 
-    if (trail.length>50) {
+    if (trail.length>100) {
     trail.shift();
     }
   
 //set up the logic
 
-  if (starCount>1300){
+  if (starCount>650){
     magicStage=4; //meat/tofu
-  } else if (starCount > 1000) {
+  } else if (starCount > 500) {
     magicStage =3; // Veggies
-  } else if (starCount > 550) {
+  } else if (starCount > 350) {
     magicStage = 2; // Noodles
-  } else if (starCount > 250) {
+  } else if (starCount > 200) {
     magicStage = 1; // Bowl
   }
 drawTrail();
@@ -113,27 +113,48 @@ function drawThaiFood(x,y,stage){
 if (stage >= 1) {
     fill(200, 50, 50); // Bright Red Bowl
     noStroke();
-    arc(0, 0, 200, 150, 0, PI, CHORD);
+    arc(0, 0, 240, 170, 0, PI, CHORD);
   }
   //stage 2
   if (stage >= 2) {
-    fill(255, 200, 100); // Golden Noodles
-    ellipse(0, 0, 190, 50);
+   stroke(255, 200, 100); 
+    strokeWeight(2);
+    noFill();
+    for (let i = 0; i < 20; i++) {
+      let x1 = -90; 
+      let x2 = 90;
+      let yStrand = map(i, 0, 19, -30, 10);
+      beginShape();
+      // Using curves instead for the shape
+      curveVertex(x1, yStrand - 20);
+      curveVertex(x1 + random(-5,5), yStrand); 
+      curveVertex(0 + random(-10,10), yStrand + random(-10,10)); 
+      curveVertex(x2 + random(-5,5), yStrand); 
+      curveVertex(x2, yStrand - 20); 
+      endShape();
+    }
   }
+  noStroke();
+  
 //stage 3
   if (stage >= 3) {
     fill(50, 150, 50); // Fresh Green
     circle(-20, -5, 15);
     circle(10, -10, 10);
     circle(30, 5, 12);
+    circle(14, 2, 8);
+    circle(20, 10, 10);
+    circle(-20, -5, 20);
   }
   //stage 4 tofu/meat
   if (stage >= 4) {
-    fill(230, 190, 150); // Tofu color
+    fill(230, 170, 150); // Tofu color
     rectMode(CENTER);
     square(-10, 5, 20);
     square(25, 10, 18);
     square(5, -15, 15);
+     square(20, 15, 19);
+     square(15, 5, 18);
   }
   pop();
 }
