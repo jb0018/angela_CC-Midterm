01152d37afe3288b16753a951becb9e661ceb69f
Angela B <jb8161@nyu.edu>
2026-03-13 11:47:10 -0400

    Step 1: Added magical match with timing

--- sketch.js
+++ sketch.js
@@ -1,10 +1,37 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let magicLevel = 0; 
+let isBursting = false; // Tracks if the magic is happening
+
+function mouseClicked() {
+  magicLevel++;
+  
+  // 1.The match glows brighter
+  console.log("Match struck!");
+
+  // 2. Delayed Action, maybe around 1.5s trigger the burst
+  setTimeout(function() {
+    triggerMagicBurst();
+  }, 1500);
 }
 
-function draw() {
-  background(30); // Dark grey
-  fill(0, 255, 150); // Teal green
-  noStroke();
-  circle(mouseX, mouseY, 50);
+function triggerMagicBurst() {
+  isBursting = true;
+  console.log("The magic manifested!");
+  
+  // let the burst disappear
+  setTimeout(function() {
+    isBursting = false;
+  }, 3000);
 }
+
+ function draw() {
+  background(10, 15, 30);
+
+  // --- DRAW THE MATCH as cursor ---
+  drawMatch(mouseX, mouseY);
+
+  // --- draw the delayed effect ---
+  if (isBursting) {
+    drawMagicalAura();
+  }
+}
+
